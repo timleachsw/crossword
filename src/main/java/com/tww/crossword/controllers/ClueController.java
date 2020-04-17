63e7eb03c282a6e7e8c6a1ca94cbd0f1dabe3869
timleachsw <Tim.Leach@softwire.com>
2020-04-17 16:31:02 +0100

    Add POST endpoint for creating clues

--- src/main/java/com/tww/crossword/controllers/ClueController.java
+++ src/main/java/com/tww/crossword/controllers/ClueController.java
@@ -1,14 +1,39 @@
 package com.tww.crossword.controllers;
 
+import com.tww.crossword.models.Clue;
+import com.tww.crossword.repositories.ClueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path = "/clue")
 public class ClueController {
 
+    @Autowired
+    private ClueRepository clueRepository;
+
     @GetMapping
     public @ResponseBody String sayHello() { return "Hello"; }
+
+    @PostMapping(path="/add")
+    public @ResponseBody
+    String addClue (
+            @RequestParam String clue,
+            @RequestParam String description,
+            @RequestParam String topic,
+            @RequestParam String style,
+            @RequestParam String author,
+            @RequestParam Integer difficulty
+    ) {
+        Clue newClue = new Clue();
+        newClue.setClue(clue);
+        newClue.setDescription(description);
+        newClue.setTopic(topic);
+        newClue.setStyle(style);
+        newClue.setAuthor(author);
+        newClue.setDifficulty(difficulty);
+        clueRepository.save(newClue);
+        return "Added clue";
+    }
 }
