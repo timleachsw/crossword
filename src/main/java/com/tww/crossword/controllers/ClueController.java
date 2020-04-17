c17687c4ecd09b119cacb3c75d47fb353048c79a
Will Powell <William.Powell@softwire.com>
2020-04-17 16:09:29 +0100

    added our first endpoint!

---
+++ src/main/java/com/tww/crossword/controllers/ClueController.java
@@ -0,0 +1,15 @@
+package com.tww.crossword.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/clue")
+public class ClueController {
+
+    @GetMapping
+    public @ResponseBody
+    String sayHello() { return "Hello"; }
+}
