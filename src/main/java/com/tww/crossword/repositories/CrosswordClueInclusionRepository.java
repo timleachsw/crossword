58602b1f4ae3bc75fe738983088536b14e36273f
timleachsw <Tim.Leach@softwire.com>
2020-04-24 15:08:35 +0100

    Create corresponding repositiories for the new models

---
+++ src/main/java/com/tww/crossword/repositories/CrosswordClueInclusionRepository.java
@@ -0,0 +1,7 @@
+package com.tww.crossword.repositories;
+
+import com.tww.crossword.models.CrosswordClueInclusion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrosswordClueInclusionRepository extends CrudRepository<CrosswordClueInclusion, Integer> {
+}
