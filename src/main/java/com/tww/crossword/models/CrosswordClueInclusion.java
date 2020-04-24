070123417303cd8a36eb7908e31d6848c64d060e
timleachsw <Tim.Leach@softwire.com>
2020-04-24 15:17:44 +0100

    Set fields of CrosswordClueInclusion correctly

--- src/main/java/com/tww/crossword/models/CrosswordClueInclusion.java
+++ src/main/java/com/tww/crossword/models/CrosswordClueInclusion.java
@@ -8,12 +8,61 @@ public class CrosswordClueInclusion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String name;
-    private String author;
+    private Integer xPosition;
+    private Integer yPosition;
+    private String direction;
 
     @ManyToOne
     private Crossword crossword;
 
     @ManyToOne
     private Clue clue;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition(Integer xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public Integer getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition(Integer yPosition) {
+        this.yPosition = yPosition;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public Crossword getCrossword() {
+        return crossword;
+    }
+
+    public void setCrossword(Crossword crossword) {
+        this.crossword = crossword;
+    }
+
+    public Clue getClue() {
+        return clue;
+    }
+
+    public void setClue(Clue clue) {
+        this.clue = clue;
+    }
 }
